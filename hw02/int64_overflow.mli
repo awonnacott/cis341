0a228bb90bda752227f1f215ec7e278061271370
Andrew Wonnacott <awonnacott@princeton.edu>
2019-05-22 17:31:14 -0400

    cos320sp19 handout code

---
+++ hw02/int64_overflow.mli
@@ -0,0 +1,14 @@
+(* Author: Steve Zdancewic  *)
+exception Overflow
+
+type t = { value : int64; overflow : bool }
+
+val ok : int64 -> t
+
+val neg : int64 -> t
+val succ : int64 -> t
+val pred : int64 -> t
+
+val add : int64 -> int64 -> t
+val sub : int64 -> int64 -> t
+val mul : int64 -> int64 -> t
